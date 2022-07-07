ddbc4dcd9b94a7b51dd33a153e1f814e9487464c
Sam Foit <smfoit4311@gmail.com>
2022-07-07 17:43:10 -0500

    Added scoreboard

--- game.js
+++ game.js
@@ -1,16 +1,20 @@
+let playerWins = 0;
+let computerWins = 0;
+
+// gets every button
 const buttons = document.querySelectorAll('input');
 
+// creates a function for each button that accesses their data-value key and plays a round
 buttons.forEach(input => input.addEventListener('click', () => {
     playRound(input.dataset.value);
-}))
+}));
 
-// 0: rock, 1: paper, 2: scissors
 function computerPlay(){
     return Math.floor(Math.random() * 3)
 }
 
 function playRound(playerSelection){
-    const computerSelection = computerPlay();
+    let computerSelection = computerPlay();
     
     console.log(`Player chose ${playerSelection}`);
     console.log(`Comp chose ${computerSelection}`);
@@ -19,10 +23,37 @@ function playRound(playerSelection){
         console.log('It was a tie!');
     }
     else if (computerSelection == playerSelection + 1 || computerSelection == playerSelection - 2){
-        console.log('You lose...');
+        score(false);
     }
     else {
-        console.log('You win!');
+        score(true);
+    }
+}
+
+function score(playerWon){
+    const score = document.querySelector('h1');
+    if (playerWon) {
+        playerWins = playerWins + 1;
+        score.textContent = `${playerWins}-${computerWins}`;
+        checkWin(playerWins, true);
+    }
+    else {
+        computerWins = computerWins + 1;
+        score.textContent = `${playerWins}-${computerWins}`;
+        checkWin(playerWins, true);
+    }
+}
+
+function checkWin(wins, player){
+    const scoreboard = document.querySelector('h1');
+
+    if (wins >= 5){
+        if (player){
+            scoreboard.textContent = 'You Win!';
+        }
+        else {
+            scoreboard.textContent = 'You Lost...';
+        }
     }
 }
 
