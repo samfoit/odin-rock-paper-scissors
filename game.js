dee2c0f46f51cb74479b2f97ca74bb439fae9f96
Sam Foit <smfoit4311@gmail.com>
2022-07-27 15:15:56 -0500

    Fixed losing bug

--- game.js
+++ game.js
@@ -37,7 +37,7 @@ function score(playerWon){
     else {
         computerWins = computerWins + 1;
         score.textContent = `${playerWins}-${computerWins}`;
-        checkWin(playerWins, true);
+        checkWin(computerWins, false);
     }
 }
 
@@ -47,9 +47,11 @@ function checkWin(wins, player){
     if (wins >= 5){
         if (player){
             scoreboard.textContent = 'You Win!';
+            deleteButtons();
         }
         else {
             scoreboard.textContent = 'You Lost...';
+            deleteButtons();
         }
     }
 }
@@ -58,4 +60,14 @@ function game(){
     for (let i = 0; i < 5; i++){
         console.log(playRound(prompt("Please enter Rock, Paper, or Scissors"),computerPlay()));
     }
+}
+
+function deleteButtons(){
+    const fire = document.querySelector('#fire');
+    const water = document.querySelector('#water');
+    const earth = document.querySelector('#earth');
+
+    fire.remove();
+    water.remove();
+    earth.remove();
 }
